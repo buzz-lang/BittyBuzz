bc07c2c247d3e58a456d46f5db3d635973c17a79
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2017-08-23 11:29:10 -0400

    Updated the kilobot's blank script.

--- src/kilobot/lib/bbzblank.c
+++ src/kilobot/lib/bbzblank.c
@@ -6,39 +6,6 @@
 PROGMEM const uint8_t bcode[] = {(uint8_t)(_BBZSTRID_COUNT_),(uint8_t)((_BBZSTRID_COUNT_)>>8),0,1};
 PROGMEM const uint16_t bcode_size = sizeof(bcode);
 
-void ___led(uint8_t x) {set_color(x); delay(50); set_color(0); delay(100);}
-
-void err_receiver(bbzvm_error errcode) {
-    set_motors(0,0);
-    uint8_t i;
-    for (i = 4; i; --i) {
-        ___led(RGB(1,2,0));
-    }
-    delay(300);
-#if 1
-    for (i = 4; i; --i) {
-        delay(700);
-        switch(errcode) {
-            case BBZVM_ERROR_INSTR:  ___led(RGB(2,0,0)); ___led(RGB(2,0,0)); break;
-            case BBZVM_ERROR_STACK:  ___led(RGB(1,2,0)); if (bbzvm_stack_size() >= BBZSTACK_SIZE) { ___led(RGB(0,3,0)); } else if (bbzvm_stack_size() <= 0) { ___led(RGB(2,0,0)); } else { ___led(RGB(1,2,0)); } break;
-            case BBZVM_ERROR_LNUM:   ___led(RGB(3,1,0)); ___led(RGB(3,1,0)); break;
-            case BBZVM_ERROR_PC:     ___led(RGB(0,3,0)); ___led(RGB(0,3,0)); break;
-            case BBZVM_ERROR_FLIST:  ___led(RGB(0,3,0)); ___led(RGB(2,0,0)); break;
-            case BBZVM_ERROR_TYPE:   ___led(RGB(0,3,0)); ___led(RGB(1,2,0)); break;
-            case BBZVM_ERROR_RET:    ___led(RGB(0,3,0)); ___led(RGB(0,0,2)); break;
-            case BBZVM_ERROR_STRING: ___led(RGB(0,2,1)); ___led(RGB(0,2,1)); break;
-            case BBZVM_ERROR_SWARM:  ___led(RGB(0,2,1)); ___led(RGB(2,0,0)); break;
-            case BBZVM_ERROR_VSTIG:  ___led(RGB(0,2,1)); ___led(RGB(1,2,0)); break;
-            case BBZVM_ERROR_MEM:    ___led(RGB(0,2,1)); ___led(RGB(0,0,2)); break;
-            case BBZVM_ERROR_MATH:   ___led(RGB(0,0,2)); ___led(RGB(0,0,2)); break;
-            default: ___led(RGB(2,0,2)); ___led(RGB(2,0,2)); break;
-        }
-    }
-#endif
-    ___led(RGB(2,2,2));
-    ___led(RGB(2,2,2));
-}
-
 void loop() {
     bbzvm_gc();
     set_color(RGB(3,0,0));
@@ -63,7 +30,6 @@ void loop() {
 }
 
 void setup() {
-    bbzvm_set_error_receiver(err_receiver);
     bbzvm_function_register(__BBZSTRID_step, loop);
 }
 
